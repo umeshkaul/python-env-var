3ef1d09b06eaf3e6c49ec2a1b8a67da93517bdde
Umesh Kaul <umeshkaul@gmail.com>
2020-01-13 12:31:06 -0500

    added some comments

--- getenvar.py
+++ getenvar.py
@@ -1,7 +1,7 @@
 import os
 import pprint
 env_var = os.environ
-print("Users environment variables:")
+print("This script prints Users environment variables:")
 
 pprint.pprint(dict(env_var),width=1)
 print("\n\n Users PATH : ")
